fa5a02c9692f490df506460212d8bbcf0a3c4fd9
sofia-bjorkesjo <sb223fz@student.lnu.se>
2018-02-08 14:01:43 +0100

    update works

--- routes/main.js
+++ routes/main.js
@@ -12,7 +12,6 @@ router.get('/', function(req, res) {
     res.send(links);
 
 }).post('/', function(req, res) {
-    console.log(req.body[0].baker); 
     let cake = new cakeSchema({
         sortOfCake: req.body[0].sortOfCake,
         baker: req.body[0].baker,
@@ -30,8 +29,25 @@ router.get('/', function(req, res) {
     })
 });
 
+router.get('/:cakeId', function(req, res) {
+    cakeSchema.findOne({_id: req.params.cakeId}, function(err, cake) {
+        if(err) {
+            res.send(err);
+        } else {
+            res.json(cake);
+        }
+    });
+});
+
 router.put('/:cakeId', function(req, res) {
-    cakeSchema.findOneAndUpdate({_id: req.params.cakeId}, {baker: 'hej'}, function(err, cake) {
+    cakeSchema.findOneAndUpdate({_id: req.params.cakeId},
+        {sortOfCake: req.body[0].sortOfCake,
+        baker: req.body[0].baker,
+        sizeOfCake: req.body[0].sizeOfCake,
+        date: req.body[0].date,
+        imageURL: req.body[0].imageURL,
+        ingredients: req.body[0].ingredients},
+        function(err, cake) {
         if(err) {
             res.send(err);
         } else {
@@ -77,6 +93,63 @@ router.get('/cakes/:cakeId', function(req, res) {
             res.send(err);
         } else {
             res.send(information);
+        }
+    });
+});
+
+router.get('/bakers', function(req, res) {
+    cakeSchema.find({}, function(err, bakers) {
+        if(err) {
+            res.send(err);
+        } else {
+            let bakersMap = [];
+            bakers.forEach(function(obj){       
+                let baker = obj.baker;
+                let link = 'http://localhost:8000/bakers/' + obj.baker;
+
+                let info = {
+                    'baker': baker,
+                    'All the bakers cakes': link
+                };
+
+                bakersMap.push(info); 
+            })
+            let test = [];
+
+            // for(let i = 0; i < bakersMap.length; i++) {
+            //     for(let j = 0; j <= test.length; j++) {
+            //         if(test.length == 0){
+            //             test.push(bakersMap[i]); 
+            //         } 
+            //         if(bakersMap[i].baker == test[j].baker) {
+            //             console.log('sssss');
+            //         } else {
+            //             test.push(bakersMap[i]);
+            //         }
+            //     }
+                
+            // }
+            res.send(bakersMap);
+        }
+    })
+});
+
+router.get('/bakers/:bakerName/', function(req, res) {
+    cakeSchema.find({baker: req.params.bakerName}, function(err, information) {
+        if(err) {
+            res.send(err);
+        } else {
+            let allCakes = [];
+            information.forEach(function(cake) {
+                console.log(cake.sortOfCake);
+                let result = {
+                    'baker': cake.baker,
+                    'cake': cake.sortOfCake
+                }
+               
+                allCakes.push(result);
+            });
+            res.send(allCakes);
         }
     });
 });
